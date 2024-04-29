a451225a2c9eb30505c26a8c581c766d9b4b563a
zulaeha168 <130324650+zulaeha168@users.noreply.github.com>
2024-04-29 16:05:42 +0700

    Delete Lab4_php_modular directory

--- Lab4_php_modular/about.php
+++
@@ -1,8 +0,0 @@
-<?php require('header.php'); ?>
-
-<div class="content">
-    <h2>Ini Halaman About</h2>
-    <p>Ini adalah content dari halaman.</p>
-</div>
-
-<?php require('footer.php');?>
--- Lab4_php_modular/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>&copy; 2024, Teknik Informatika, Universitas Pelita Bangsa</p>
-</footer>
-</div>
-</body>
-</html>
